74d07f01ea8e89cdcd81892d5637e481015a27e9
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2023-05-07 18:22:50 +0100

    images test

--- src/pages/index.js
+++ src/pages/index.js
@@ -70,9 +70,55 @@ const moreLinks = [
 const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
 
 const IndexPage = () => (
-  <Layout>
-    <div className={styles.textCenter}>
-      <StaticImage
+  // <Layout>
+  //   <div className={styles.textCenter}>
+  //     <StaticImage
+  //       src="../images/example.png"
+  //       loading="eager"
+  //       width={64}
+  //       quality={95}
+  //       formats={["auto", "webp", "avif"]}
+  //       alt=""
+  //       style={{ marginBottom: `var(--space-3)` }}
+  //     />
+  //     <img src="../images/example.png" alt="nothing" />
+  //     <h1>
+  //       Welcome to <b>Gatsby!</b>
+  //     </h1>
+  //     <p className={styles.intro}>
+  //       <b>Example pages:</b>{" "}
+  //       {samplePageLinks.map((link, i) => (
+  //         <React.Fragment key={link.url}>
+  //           <Link to={link.url}>{link.text}</Link>
+  //           {i !== samplePageLinks.length - 1 && <> · </>}
+  //         </React.Fragment>
+  //       ))}
+  //       <br />
+  //       Edit <code>src/pages/index.js</code> to update this page.
+  //     </p>
+  //   </div>
+  //   <ul className={styles.list}>
+  //     {links.map(link => (
+  //       <li key={link.url} className={styles.listItem}>
+  //         <a
+  //           className={styles.listItemLink}
+  //           href={`${link.url}${utmParameters}`}
+  //         >
+  //           {link.text} ↗
+  //         </a>
+  //         <p className={styles.listItemDescription}>{link.description}</p>
+  //       </li>
+  //     ))}
+  //   </ul>
+  //   {moreLinks.map((link, i) => (
+  //     <React.Fragment key={link.url}>
+  //       <a href={`${link.url}${utmParameters}`}>{link.text}</a>
+  //       {i !== moreLinks.length - 1 && <> · </>}
+  //     </React.Fragment>
+  //   ))}
+  // </Layout>
+  <div>
+    <StaticImage
         src="../images/example.png"
         loading="eager"
         width={64}
@@ -80,42 +126,9 @@ const IndexPage = () => (
         formats={["auto", "webp", "avif"]}
         alt=""
         style={{ marginBottom: `var(--space-3)` }}
-      />
-      <h1>
-        Welcome to <b>Gatsby!</b>
-      </h1>
-      <p className={styles.intro}>
-        <b>Example pages:</b>{" "}
-        {samplePageLinks.map((link, i) => (
-          <React.Fragment key={link.url}>
-            <Link to={link.url}>{link.text}</Link>
-            {i !== samplePageLinks.length - 1 && <> · </>}
-          </React.Fragment>
-        ))}
-        <br />
-        Edit <code>src/pages/index.js</code> to update this page.
-      </p>
-    </div>
-    <ul className={styles.list}>
-      {links.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-          >
-            {link.text} ↗
-          </a>
-          <p className={styles.listItemDescription}>{link.description}</p>
-        </li>
-      ))}
-    </ul>
-    {moreLinks.map((link, i) => (
-      <React.Fragment key={link.url}>
-        <a href={`${link.url}${utmParameters}`}>{link.text}</a>
-        {i !== moreLinks.length - 1 && <> · </>}
-      </React.Fragment>
-    ))}
-  </Layout>
+    />
+    <img src="../images/example.png" alt="nope" />
+  </div>
 )
 
 /**
